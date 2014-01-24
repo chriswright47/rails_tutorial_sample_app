46fccb9f4827d0a64daa51f3b78b32f69367d593
Chris Wright <christopher.wright47@gmail.com>
2014-01-24 11:57:27 -0800

    refactored tests to use route helpers

--- spec/features/static_pages_spec.rb
+++ spec/features/static_pages_spec.rb
@@ -5,48 +5,48 @@ describe "StaticPages" do
 
   describe "Home page" do
     it "has the content 'Sample App'" do
-      visit '/static_pages/home'
+      visit root_path
       expect(page).to have_selector('h1', text: 'Sample App')
     end
     it 'has the base title' do
-      visit '/static_pages/home'
+      visit root_path
       expect(page).to have_title "#{base_title}"
     end
     it 'should not have custom title' do
-      visit '/static_pages/home'
+      visit root_path
       expect(page).to_not have_title "| Home"
     end
   end
 
   describe 'Help page' do
     it "has the content 'Help'" do
-      visit 'static_pages/help'
+      visit help_path
       expect(page).to have_selector('h1', text: 'Help')
     end
     it "should have the title 'Help'" do
-      visit '/static_pages/help'
+      visit help_path
       expect(page).to have_title "#{base_title} | Help"
     end
   end
 
   describe 'About page' do
     it "has the content 'About us'" do
-      visit '/static_pages/about'
+      visit about_path
       expect(page).to have_selector('h1', text: 'About us')
     end
     it 'has the right title' do
-      visit '/static_pages/about'
+      visit about_path
       expect(page).to have_title "#{base_title} | About Us"
     end
   end
 
   describe 'Contact page' do
     it "has the content 'Contact us'" do
-      visit '/static_pages/contact'
+      visit contact_path
       expect(page).to have_selector('h1', text: 'Contact us')
     end
     it "has the right title" do
-      visit 'static_pages/contact'
+      visit contact_path
       expect(page).to have_title "#{base_title} | Contact Us"
     end
   end
