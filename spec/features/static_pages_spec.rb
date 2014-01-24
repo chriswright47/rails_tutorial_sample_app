23268bf3a5ba9107f0f67fae0efb20f070c1d6f0
Chris Wright <christopher.wright47@gmail.com>
2014-01-24 12:19:21 -0800

    refactored tests more

--- spec/features/static_pages_spec.rb
+++ spec/features/static_pages_spec.rb
@@ -2,47 +2,34 @@ require 'spec_helper'
 
 describe "StaticPages" do
   let(:base_title) {"RoR Tutorial"}
+  subject { page }
 
   describe "Home page" do
     before { visit root_path }
-    it "has the content 'Sample App'" do
-      expect(page).to have_selector('h1', text: 'Sample App')
-    end
-    it 'has the base title' do
-      expect(page).to have_title "#{base_title}"
-    end
-    it 'should not have custom title' do
-      expect(page).to_not have_title "| Home"
-    end
+
+    it { should have_selector('h1', text: 'Sample App') }
+    it { should have_title full_title('') }
+    it { should_not have_title '| Home' }
   end
 
   describe 'Help page' do
     before { visit help_path }
-    it "has the content 'Help'" do
-      expect(page).to have_selector('h1', text: 'Help')
-    end
-    it "should have the title 'Help'" do
-      expect(page).to have_title "#{base_title} | Help"
-    end
+
+    it { should have_selector('h1', text: 'Help') }
+    it { should have_title full_title('Help') }
   end
 
   describe 'About page' do
     before { visit about_path }
-    it "has the content 'About us'" do
-      expect(page).to have_selector('h1', text: 'About us')
-    end
-    it 'has the right title' do
-      expect(page).to have_title "#{base_title} | About Us"
-    end
+
+    it { should have_selector('h1', text: 'About us') }
+    it { should have_title full_title('About Us') }
   end
 
   describe 'Contact page' do
     before { visit contact_path }
-    it "has the content 'Contact us'" do
-      expect(page).to have_selector('h1', text: 'Contact us')
-    end
-    it "has the right title" do
-      expect(page).to have_title "#{base_title} | Contact Us"
-    end
+
+    it { should have_selector('h1', text: 'Contact us') }
+    it { should have_title full_title('Contact Us') }
   end
 end
