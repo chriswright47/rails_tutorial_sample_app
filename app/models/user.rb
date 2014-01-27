b07b6a03806863a99e9c5e1aa3848d7396f43df7
Chris Wright <christopher.wright47@gmail.com>
2014-01-27 13:37:06 -0800

    added downcase_email on users before save

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,6 @@ class User < ActiveRecord::Base
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
 
+  before_save { |user| user.email = email.downcase }
+
 end
