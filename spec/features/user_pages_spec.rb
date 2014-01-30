182e87a8fbd7fac298816bf5fedb4da2872794de
Chris Wright <christopher.wright47@gmail.com>
2014-01-29 19:14:31 -0800

    wrote tests for new user form page

--- spec/features/user_pages_spec.rb
+++ spec/features/user_pages_spec.rb
@@ -6,9 +6,28 @@ describe 'User pages' do
 
   describe 'new user page' do
     before { visit new_user_path }
+    let(:submit) { 'Create my Account' }
 
     it { should have_selector('h1', text: 'Sign up') }
     it { should have_title full_title('Sign up') }
+
+    describe 'with invalid information' do
+      it ' should not create a user' do
+        expect { click_button submit }.not_to change(User, :count)
+      end
+    end
+
+    describe 'with valid information' do
+      before do
+        fill_in 'Name', with: 'Example User'
+        fill_in 'Email', with: 'user@example.com'
+        fill_in 'Password', with: 'foobar'
+        fill_in 'Confirmation', with: 'foobar'
+      end
+      it 'should create a user' do
+        expect { click_button submit }.to change(User, :count).by(1)
+      end
+    end
   end
 
   describe 'profile page' do
