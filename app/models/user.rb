0cef16257e15b390d37b9de7da7427fd10ccb8c3
Chris Wright <christopher.wright47@gmail.com>
2014-02-03 11:02:31 -0800

    sign in and sign out functionality are complete, with conditional links in header relying on helper methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,6 +18,6 @@ class User < ActiveRecord::Base
   private
 
     def create_remember_token
-      self.remeber_token = SecureRandom.urlsafe_base64
+      self.remember_token = SecureRandom.urlsafe_base64
     end
 end
